883ce3c027f28b608fb5b778942b533e2cd1ecd1
R2 D2 <p234a137@gmail.com>
2015-01-17 22:05:48 -0800

    select features, mean and std using regex

--- run_analysis.R
+++ run_analysis.R
@@ -9,14 +9,16 @@ list.files("UCI HAR Dataset/")
 # You should create one R script called run_analysis.R that does the following. 
 # 1- Merges the training and the test sets to create one data set.
 X_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-X_test <- read.table("UCI HAR Dataset/test/X_test.txt"))
+X_test <- read.table("UCI HAR Dataset/test/X_test.txt")
 X_all <- rbind(X_train, X_test)
 
 # 2- Extracts only the measurements on the mean and standard deviation for each measurement. 
-features <- read.table("UCI HAR Dataset//features.txt")
+features <- read.table("UCI HAR Dataset//features.txt", colClasses = "character")
 feature_names = features[,2]
 names(X_all) <- feature_names
-
+selected_indices <- grep("-mean\\(\\)|-std\\(\\)", feature_names)
+selected_features <- feature_names[selected_indices]
+X_selected <- X_all[selected_features]
 
 # 3- Uses descriptive activity names to name the activities in the data set
 # 4- Appropriately labels the data set with descriptive variable names. 
